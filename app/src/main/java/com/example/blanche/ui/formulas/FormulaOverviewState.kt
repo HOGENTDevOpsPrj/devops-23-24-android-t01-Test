0e7700338749560604cdf15ed2dc8d76ee346199
aaronpollet <aaron.pollet@student.hogent.be>
2023-12-04 19:34:34 +0100

    build error fix

--- app/src/main/java/com/example/blanche/ui/formulas/FormulaOverviewState.kt
+++ app/src/main/java/com/example/blanche/ui/formulas/FormulaOverviewState.kt
@@ -18,10 +18,8 @@ data class FormulaListState(val formulaList: List<Formula> = listOf())
 
 // the sealed interface has only three possible values
 /*Sidenote: to learn more about this TaskApiState object, you can search on LCE (Loading, Content, Error) pattern
-
 When the state is changed to Error, the taskList will not be updated (offline first).
 To ensure the list is considered immutable (fully immutable, won't ever change unless a new object is created), add the Immutable annotation.
-
 The LCE pattern is not completed in the application, because it requires more complex helper classes
 An example can be found here https://www.valueof.io/blog/compose-ui-state-flow-offline-first-repository
 */
