0c867eb830c82b11cd24ba7054b951c157984c2b
Jarno <jarno_raes@msn.com>
2023-12-16 17:01:30 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,13 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
+
+kotlin {
+    jvm {
+        sourceSets {
+            main {
+                kotlin.srcDirs += 'src/main/kotlin'
+            }
+        }
+    }
+}
