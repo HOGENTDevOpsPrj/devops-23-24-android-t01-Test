9ba99b86063f9cdbf4c6b4200e7c214a984b0018
Jarno <jarno_raes@msn.com>
2023-12-18 00:14:13 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
- compileJava.options.fork = true
- compileJava.options.forkOptions.executable = '/usr/lib/jvm/java-17-openjdk-17.0.9.0.9-2.el9.x86_64'
-
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
